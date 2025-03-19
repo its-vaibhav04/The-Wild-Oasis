3032529965dce38c9d1d53191a42183e78f67e1e
its-vaibhav04 <tyagi.vaibhav4814@gmail.com>
2025-03-19 19:34:37 +0530

    Added React-Query

--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -1,19 +1,18 @@
 import styled, { css } from "styled-components";
-
 const Row = styled.div`
   display: flex;
-  ${(props) =>
-    props.type === "horizontal" &&
-    css`
-      justify-content: space-between;
-      align-items: center;
-    `}
-  ${(props) =>
-    props.type === "vertical" &&
-    css`
-      flex-direction: column;
-      gap: 1.6rem;
-    `}
+  ${(props) => {
+    const type = props.type || "vertical";
+    return type === "horizontal"
+      ? css`
+          justify-content: space-between;
+          align-items: center;
+        `
+      : css`
+          flex-direction: column;
+          gap: 1.6rem;
+        `;
+  }}
 `;
 
 export default Row;
